17c57e8e9fd097328a38be0531d9c6b2ff703b0d
werpu <werner.punz@gmail.com>
2011-02-01 23:21:32 +0100

    reducing the component down to the bare minimum

--- src/main/scala/org/extrasapache/myfaces/codi/examples/ebean/support/ui/Component.scala
+++ src/main/scala/org/extrasapache/myfaces/codi/examples/ebean/support/ui/Component.scala
@@ -14,38 +14,26 @@ import java.io.Serializable
  */
 package paginator {
 
-  object Const extends Serializable {
-    val FOR_TABLE = "forTable"
-    val VALUE = "value"
-    val FOR_TABLE_CLIENTID = "forTableClientId"
-    val ENCLOSING_CONTAINER_CLIENT_ID = "forTableClientId"
-    val PAGING_CONTROLLER = "pagingController"
-  }
+object Const extends Serializable {
+  val FOR_TABLE = "forTable"
+  val VALUE = "value"
+  val FOR_TABLE_CLIENTID = "forTableClientId"
+  val ENCLOSING_CONTAINER_CLIENT_ID = "enclosingContainerClientId"
+  val PAGING_CONTROLLER = "pagingController"
+}
 
-  @FacesComponent("at.irian.PaginatorComponent")
-  class Component extends UINamingContainer {
+@FacesComponent("at.irian.PaginatorComponent")
+class Component extends UINamingContainer {
 
-    def beforeEncode(event: ComponentSystemEvent) = {
-      def forTable = this.getParent().findComponent(getForTable).asInstanceOf[HtmlDataTable]
-      setForTableClientId(forTable.getClientId)
-      setEnclosingContainerClientId(this.findComponent(Const.PAGING_CONTROLLER).getClientId);
+  def beforeEncode(event: ComponentSystemEvent) = {
+    def putAttr(key: String, entry: String) {
+      getAttributes.put(key, entry)
     }
 
-    def getValue: PaginationController[_] = getStateHelper().eval(Const.VALUE, null).asInstanceOf[PaginationController[_]]
-
-    def getForTable: String = getStateHelper().eval(Const.FOR_TABLE, "").asInstanceOf[String]
-
-    def getForTableClientId: String = getStateHelper().eval(Const.FOR_TABLE_CLIENTID, "").asInstanceOf[String]
-
-    def getEnclosingContainerClientId: String = getStateHelper().eval(Const.ENCLOSING_CONTAINER_CLIENT_ID, "").asInstanceOf[String]
-
-    def setValue(value: PaginationController[_]): Unit = getStateHelper().put(Const.VALUE, value)
-
-    def setForTable(theVal: String): Unit = getStateHelper().put(Const.FOR_TABLE, theVal)
-
-    def setForTableClientId(value: String): Unit = getStateHelper().put(Const.FOR_TABLE_CLIENTID, value)
-
-    def setEnclosingContainerClientId(value: String): Unit = getStateHelper().put(Const.ENCLOSING_CONTAINER_CLIENT_ID, value)
+    def forTable = this.getParent().findComponent(getAttributes.get(Const.FOR_TABLE).asInstanceOf[String]).asInstanceOf[HtmlDataTable]
+    putAttr(Const.FOR_TABLE_CLIENTID,forTable.getClientId)
+    putAttr(Const.ENCLOSING_CONTAINER_CLIENT_ID, this.findComponent(Const.PAGING_CONTROLLER).getClientId);
   }
+}
 
 }
