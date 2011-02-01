5c99eedb996fadb96da611b90a2a2548efb525cb
werpu <werner.punz@gmail.com>
2011-02-01 14:26:50 +0100

    now the entire person submodule is moved over to scala, yiehaa

---
+++ src/main/scala/org/extrasapache/myfaces/codi/examples/ebean/business/bo/person/PersonFacade.scala
@@ -0,0 +1,48 @@
+package org.extrasapache.myfaces.codi.examples.ebean.business.bo.person
+
+import org.extrasapache.myfaces.codi.examples.ebean.business.bo.common.FacadeBase
+import java.io.Serializable
+import javax.inject.Inject
+import org.extrasapache.myfaces.codi.examples.ebean.orm.person.{Address, Person}
+import org.extrasapache.myfaces.codi.examples.ebean.support.data.PaginationController
+import org.extrasapache.myfaces.codi.examples.ebean.business.util.{OrderEntry, FilterEntry}
+
+/**
+ *
+ * @author Werner Punz (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+
+class PersonFacade extends FacadeBase[Person] with Serializable {
+
+  clazz = classOf[Person]
+
+  def create: Person = new Person
+
+  def createAdr: Address = new Address
+
+  def delete(person: Person) = {
+    val newPers = if (person.getId != null) {
+      em.find(clazz, person.getId)
+    } else {
+      person
+    }
+    em.delete(newPers)
+  }
+
+  override def loadFromTo(from: Int, pageSize: Int, filter: java.util.List[FilterEntry], orderBy: java.util.List[OrderEntry]): PaginationController[Person] = {
+    val query = em.createQuery(clazz)
+    query.fetch("addresses")
+    applyFilters(query, filter, orderBy)
+
+    getPage(from, pageSize, query)
+  }
+
+  def cancel(person: Person) = {
+    if (person.getId != null) {
+      em.refresh(person)
+      em.refreshMany(person, "addresses")
+    }
+  }
+
+}
