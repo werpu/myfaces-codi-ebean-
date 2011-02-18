d86db687261cbea5712792e2185d317f6c24b45d
werpu <werner.punz@gmail.com>
2011-02-18 22:32:07 +0100

    renaming of the type ahead js

--- src/main/webapp/resources/script/InputSuggest.js
+++ src/main/webapp/resources/script/InputSuggest.js
@@ -11,7 +11,7 @@
     var _Lang = myfaces._impl._util._Lang;
     var _AjaxQueue = extras.apache.ExtendedEventQueue;
 
-    _RT.extendClass("extras.apache.TypeAhead", extras.apache.SelectionList, {
+    _RT.extendClass("extras.apache.InputSuggest", extras.apache.ComponentBase, {
         /**
          * delay until the ajax request is triggered
          */
@@ -26,10 +26,21 @@
         valueHolderId: null,
         valueHolder: null,
 
+        _suggestPart: null,
+        
         constructor_:function(args) {
             this._callSuper("constructor", args);
             this.onKeypressInput = _Lang.hitch(this, this.onKeypressInput);
             this.valueHolderId = this.valueHolderId || this.id + this.valueHolderAppendix;
+            this._suggestPart = new extras.apache.SelectionList(arguments)
+            var oldSel = this._suggestPart.onFinalSelection
+
+            this.onFinalSelection =_Lang.hitch(this, this.onFinalSelection)
+            var _t = this
+            this._suggestPart.onFinalSelection = _Lang.hitch(this._suggestPart, function() {
+                oldSel()
+                _t.onFinalSelection
+            });
         },
 
         _postInit: function() {
@@ -40,13 +51,13 @@
 
         onKeypressInput: function(evt) {
             if(evt.keyCode == this.EVT_KEY_DOWN) {
-                this.selectedLine = 0;
-                 this.placeHolder.focus();
+                this._suggestPart.selectedLine = 0;
+                this._suggestPart.focus();
             } else {
-                this.placeHolder.setAttribute("style","display","");
+                this._suggestPart.placeHolder.setAttribute("style","display","");
                 jsf.ajax.request(evt.target, evt, {
                    execute:this.rootNode.id,
-                    render:this.placeHolder.id,
+                    render:this._suggestPart.placeHolder.id,
                     ez_typeahead: true,
                     myfaces:{
                         queueSize: this.maxTypeAhead,
@@ -72,10 +83,10 @@
             }
             if(evt.keyCode == this.KEY_ESCAPE) {
                 //hide the panel
-                this.placeHolder.setAttribute("style","display","");
+                this._suggestPart.placeHolder.setAttribute("style","display","");
                 jsf.ajax.request(evt.target, evt, {
                 execute:this.rootNode.id,
-                render:this.placeHolder.id+" "+this.valueHolder.id,
+                render:this._suggestPart.placeHolder.id+" "+this.valueHolder.id,
                 ez_typeahead: true,
                 ez_typahead_show: false,
                 myfaces:{
@@ -87,7 +98,7 @@
 
         onAjaxEvent: function(evt) {
             this._callSuper("onAjaxEvent", evt);
-            if (evt.status == "success" && evt.source == (this.id + this.placeHolderAppendix)) {
+            if (evt.status == "success" && evt.source == (this.id + this._suggestPart.placeHolderAppendix)) {
                 //we have to retrigger our refresh area handling because our ajax preview area was
                 //updated
                 this._postInit();
@@ -95,12 +106,11 @@
         },
 
         onFinalSelection:function(evt) {
-            this._callSuper("onFinalSelection", evt);
             //TODO trigger ajax selection change event
-            this.placeHolder.setAttribute("style","display","none");
+            this._suggestPart.placeHolder.setAttribute("style","display","none");
             jsf.ajax.request(evt.target, evt, {
                 execute:this.rootNode.id,
-                render:this.placeHolder.id+" "+this.valueHolder.id,
+                render:this._suggestPart.placeHolder.id+" "+this.valueHolder.id,
                 ez_typeahead: true,
                 ez_typeahead_line: this.selectedLine,
                 ez_typahead_show: true,
