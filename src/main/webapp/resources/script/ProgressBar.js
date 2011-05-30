4240e4ea490812960884dd9b877ba4222122dc38
Werner Punz <werpu2@werner-punzs-macbook-air-3.local>
2011-05-30 10:16:59 +0200

    committing the progress bar api overhauls

--- src/main/webapp/resources/script/ProgressBar.js
+++ src/main/webapp/resources/script/ProgressBar.js
@@ -11,80 +11,83 @@
      * or by manual refreshes on the bar
      */
     _RT.extendClass("extras.apache.ProgressBar", extras.apache.Pull, {
-        /**
-         * if set to true auto refresh is triggered
-         */
-        periodicPull: true,
+                /**
+                 * if set to true auto refresh is triggered
+                 */
+                periodicPull: true,
 
-        thresholdLow: 33,
-        thresholdMedium: 66,
-        thresholdHigh: 95,
+                thresholdLow: 33,
+                thresholdMedium: 66,
+                thresholdHigh: 95,
+
+                _displayMatrix: [
+                    "progressFirstThird",
+                    "progressSecondThird",
+                    "progressThirdThird",
+                    "progressFinished"
+                ],
+
+                constructor_: function(args) {
+                    this._callSuper("constructor", args);
+                    this._onAjaxEvent = _Lang.hitch(this, this._onAjaxEvent);
+                    /*we dont need any unload hooks*/
+                    this.unloadAware = false;
+                },
+
+                /**
+                 * refresh called due to periodicPull being to
+                 */
+                refresh: function() {
+                    var progressText = this.rootNode.querySelector(".progressText");
+
+                    jsf.ajax.request(this.valueHolderId, null, {execute:this.valueHolderId, render:this.valueHolderId + " " + progressText.getId(), onevent:this.onAjaxEvent});
+                },
+
+                _postInit: function() {
+                    this._callSuper("_postInit", arguments);
+                },
+
+                start: function() {
+                    if (this.periodicPull) {
+                        this._callSuper("start");
+                    }
+                },
+                stop: function() {
+                    if (this.periodicPull) {
+                        this._callSuper("stop");
+                    }
+                },
 
 
-        constructor_: function(args) {
-            this._callSuper("constructor", args);
-            this._onAjaxEvent = _Lang.hitch(this, this._onAjaxEvent);
-            /*we dont need any unload hooks*/
-            this.unloadAware = false;
-        },
+                onAjaxSuccess: function(evt) {
+                    var newValue = parseInt(document.getElementById(this.valueHolderId).value);
+                    var progressHolder = this.rootNode.querySelector(".progressHolder");
+                    var progressGauge = this.rootNode.querySelector(".progressGauge");
 
-        /**
-         * refresh called due to periodicPull being to
-         */
-        refresh: function() {
-            var progressText = this.rootNode.querySelector(".progressText");
+                    progressGauge.setStyle("width", Math.round(newValue * parseFloat(progressHolder.getAttribute("offsetWidth")) / 100.0) + "px");
 
-            jsf.ajax.request(this.valueHolderId, null, {execute:this.valueHolderId, render:this.valueHolderId + " " + progressText.getId(), onevent:this._onAjaxEvent});
-        },
+                    //rule set for the gauge coloring
+                    if (newValue < this.thresholdLow) {
+                        progressGauge.removeClass(this._getToRemove("progressFirstThird")).addClass("progressFirstThird");
+                    } else if (newValue >= this.thresholdLow && newValue < this.thresholdMedium) {
+                        progressGauge.removeClass(this._getToRemove("progressSecondThird")).addClass("progressSecondThird");
+                    } else if (newValue >= this.thresholdMedium && newValue < this.thresholdHigh) {
+                        progressGauge.removeClass(this._getToRemove("progressThirdThird")).addClass("progressThirdThird");
+                    } else {
+                        progressGauge.removeClass(this._getToRemove("progressFinished")).addClass("progressFinished");
+                    }
+                },
 
-        _onAjaxEvent: function(evt) {
-            if (evt.status == "success") {
-                var newValue = parseInt(document.getElementById(this.valueHolderId).value);
-                var progressHolder = this.rootNode.querySelector(".progressHolder");
-                var progressGauge = this.rootNode.querySelector(".progressGauge");
-
-                progressGauge.setStyle("width", Math.round(newValue * parseFloat(progressHolder.getAttribute("offsetWidth")) / 100.0) + "px");
-
-                //rule set for the gauge coloring
-                if (newValue < this.thresholdLow) {
-                    progressGauge.addClass("progressFirstThird");
-                    progressGauge.removeClass("progressSecondThird");
-                    progressGauge.removeClass("progressThirdThird");
-                    progressGauge.removeClass("progressFinished");
-                } else if (newValue >= this.thresholdLow && newValue < this.thresholdMedium) {
-                    progressGauge.removeClass("progressFirstThird");
-                    progressGauge.addClass("progressSecondThird");
-                    progressGauge.removeClass("progressThirdThird");
-                    progressGauge.removeClass("progressFinished");
-                } else if (newValue >= this.thresholdMedium && newValue < this.thresholdHigh) {
-                    progressGauge.removeClass("progressFirstThird");
-                    progressGauge.removeClass("progressSecondThird");
-                    progressGauge.addClass("progressThirdThird");
-                    progressGauge.removeClass("progressFinished");
-                } else {
-                    progressGauge.removeClass("progressFirstThird");
-                    progressGauge.removeClass("progressSecondThird");
-                    progressGauge.removeClass("progressThirdThird");
-                    progressGauge.addClass("progressFinished");
+                _getToRemove: function(toShow) {
+                    var res = [];
+                    for (var cnt = this._displayMatrix.length - 1; cnt >= 0; cnt--) {
+                        if (this._displayMatrix[cnt] != toShow) {
+                            res.push(this._displayMatrix[cnt]);
+                        }
+                    }
+                    return res;
                 }
 
-            }
-        },
 
-        _postInit: function() {
-            this._callSuper("_postInit", arguments);
-        },
-
-        start: function() {
-            if (this.periodicPull) {
-                this._callSuper("start");
-            }
-        },
-        stop: function() {
-            if (this.periodicPull) {
-                this._callSuper("stop");
-            }
-        }
-
-    });
+            });
 })();
