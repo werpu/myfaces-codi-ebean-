138b9aaeddda2b6ca9154ed0db3258adba66ee2e
Werner Punz <werner.punz@gmail.com>
2011-05-31 15:14:26 +0200

    ongoing works in the toggle control area

--- src/main/webapp/resources/script/Toggle.js
+++ src/main/webapp/resources/script/Toggle.js
@@ -7,50 +7,46 @@
     var _Lang = myfaces._impl._util._Lang;
     var _AjaxQueue = extras.apache.ExtendedEventQueue;
 
-    _RT.extendClass("extras.apache.Pull", extras.apache.ComponentBase, {
+    _RT.extendClass("extras.apache.Toggle", extras.apache.ComponentBase, {
 
                 /**
                  * the refresh interval
                  */
-                open: false,
-
-                /**
-                 * the target appendix which in summary is the same
-                 * which is used if no refreshTargetId is given
-                 * the final id is refreshTargetId or
-                 * id + ":"+ appendix
-                 */
-                toggleId: null,
-
-                /**
-                 * the refresh target id override
-                 */
-                toggleAreaId: null,
-
-                valueHolderAppendix: "_valueHolder",
-                valueHolderId: null,
-
+                open: true,
 
                 constructor_:function(args) {
                     this._callSuper("constructor", args);
                     this.ontoggleClick = _Lang.hitch(this, this.ontoggleClick);
+                    this.unloadAware = false;
                 },
 
-                postInit_: function() {
-                    this._callSuper("postInit_", args);
-                    this.rootNode.querySelector("#" + this.toggleId).addEventListener("onclick", this.ontoggleClick, true);
+                _postInit: function() {
+                    this._callSuper("_postInit", arguments);
+                    this.rootNode.querySelectorAll(".toggleControl").addEventListener("click", this.ontoggleClick, true);
+                    this._toggle({});
+                },
+
+                _toggle: function(evt) {
+                    this.open = !this.open;
+
+                    if (this.open) {
+                        this.rootNode.querySelectorAll(".toggleArea").removeClass("toggleOff").removeClass("toggleOn").addClass("toggleOn");
+                        this.rootNode.querySelectorAll(".toggleControl").removeClass("toggleOff").removeClass("toggleOn").addClass("toggleOn");
+
+                        this.onToggleOn(evt);
+                    } else {
+                        this.rootNode.querySelectorAll(".toggleArea").removeClass("toggleOn").removeClass("toggleOff").addClass("toggleOff");
+                        this.rootNode.querySelectorAll(".toggleControl").removeClass("toggleOn").removeClass("toggleOff").addClass("toggleOff");
+
+                        this.onToggleOff(evt);
+                    }
                 },
 
                 ontoggleClick: function(evt) {
-                    this.open = this.open;
-                    if (this.open) {
-                        this.rootNode.querySelector("#" + this.toggleAreaId).removeClass("toggleOff").addClass("toggleOn");
-                        this.onToggleOn(evt);
-                    } else {
-                        this.rootNode.querySelector("#" + this.toggleAreaId).removeClass("toggleOn").addClass("toggleOff");
-                        this.onToggleOff(evt);
-                    }
-                    //todo notify the server as well
+                    this._toggle(evt);
+
+                    //we send a delayed toggle request to save the last state in case of many clicks
+                    jsf.ajax.request(this.id, null, {execute:"@this", render:"@none", jsf_toggle: "" + this.open, myfaces:{delay:200} });
                 },
 
                 //callbacks for event handlers which can be set from outside, so that we can intercept toggle calls
