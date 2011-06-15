86bf29e9ed6ac4ef2bd9eba20278c786ca97b1f6
Werner Punz <werner.punz@gmail.com>
2011-06-15 21:59:58 +0200

    ongoing works on the content pane

--- src/main/webapp/resources/script/layout/ContentPane.js
+++ src/main/webapp/resources/script/layout/ContentPane.js
@@ -9,10 +9,19 @@
     var _RT = myfaces._impl.core._Runtime;
 
     _RT.extendClass("extras.apache.AccordionPanel", extras.apache.ComponentBase, {
+                _NODE:myfaces._impl._dom.Node,
+                _Lang: myfaces._impl._util._Lang,
+                _RT: myfaces._impl.core._Runtime,
+
                 _externalContentUrl: null,
-                _title: null,
+                /*if set to true embedded javascrips are evaled
+                 * otherwise not*/
+                _evalExternalContent: false,
+
                 _titleNode: null,
                 _contentNode: null,
+
+
                 constructor_: function(args) {
                     this._callSuper(args);
                 },
@@ -26,6 +35,15 @@
                         //TODO fetch content and innerHTML
                         //we only use xhr level2 objects
                         //if not xhr level2 we bomb out here
+                        var xhr = _RT.getXHRObject();
+                        //now we assume for secure we have an xhr level2 object
+                        xhr.loadend = function(data) {
+                            this._contentNode.innerHTML(data.toString());
+                        };
+                        xhr.error = function(data) {
+                            throw Error("Communications error");
+                        }
+                        xhr.open("GET", this._externalContentUrl, true);
                     }
                 },
                 //callback handler which is called
