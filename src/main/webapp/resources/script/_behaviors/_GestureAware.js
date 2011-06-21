7c41ce27a50a33f98ef34da603a39dbb39f967be
Werner Punz <werner.punz@gmail.com>
2011-06-21 07:23:26 +0200

    adding gesture behaviors

---
+++ src/main/webapp/resources/script/_behaviors/_GestureAware.js
@@ -0,0 +1,21 @@
+(function () {
+    /**
+     * Safari/Chrome gesture event behaviors
+     */
+    var _RT = myfaces._impl.core._Runtime;
+
+    _RT.extendClass("extras.apache._TouchAware", extras.apache._Behavior, {
+                constructor_: function(scope) {
+                    this._callSuper("constructor_", scope);
+                },
+
+                defineBehavior: function() {
+                    var events = ["gesturestart","gesturechange","gestureend"];
+                    for (var cnt = 0; cnt < events.length; cnt++) {
+                        var event = events[cnt];
+                        (this["_on" + event]) ? this.addEventListener(event, this["_on" + event], false) : null;
+                        (this["on" + event]) ? this.addEventListener(event, this["_on" + event], false) : null;
+                    }
+                }
+            })
+})();
