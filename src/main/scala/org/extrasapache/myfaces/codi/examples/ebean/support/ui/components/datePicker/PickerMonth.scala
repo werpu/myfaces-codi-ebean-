488f0357292a43d094a5490704521ebfb753e350
Werner Punz <werner.punz@gmail.com>
2011-07-07 22:23:39 +0200

    tests now working as expected

--- src/main/scala/org/extrasapache/myfaces/codi/examples/ebean/support/ui/components/datePicker/PickerMonth.scala
+++ src/main/scala/org/extrasapache/myfaces/codi/examples/ebean/support/ui/components/datePicker/PickerMonth.scala
@@ -1,6 +1,8 @@
 package org.extrasapache.myfaces.codi.examples.ebean.support.ui.components.datePicker
 
 import java.util.{ArrayList, Calendar}
+import java.util.logging.Logger
+
 /**
  *
  * @author Werner Punz (latest modification by $Author$)
@@ -8,6 +10,9 @@ import java.util.{ArrayList, Calendar}
  */
 @serializable
 case class PickerMonth(var selectedDay: Calendar) {
+
+  @transient
+  var logger = Logger.getLogger("PickerMonth")
 
   @transient
   var data = prepareMonth(selectedDay)
@@ -82,25 +87,27 @@ case class PickerMonth(var selectedDay: Calendar) {
 
     //not iterate over all days and create the day entry list
     val firstDay = currentMonth.getActualMinimum(Calendar.DAY_OF_MONTH)
-
+    val lastDay = currentMonth.getActualMaximum(Calendar.DAY_OF_MONTH)
     val firstDayDisplayed = makeMonth(currentMonth);
     firstDayDisplayed.set(Calendar.DAY_OF_MONTH, firstDay)
 
     val lastDayDisplayed = makeMonth(currentMonth);
-    lastDayDisplayed.set(Calendar.DAY_OF_MONTH, firstDay)
+    lastDayDisplayed.set(Calendar.DAY_OF_MONTH, lastDay)
 
     //we now skip to the beginning of the week according to the locale set
+    val DAY_LENGTH = 1000l*60l*60l*24l
     while(firstDayDisplayed.get(Calendar.DAY_OF_WEEK) != 1) {
-      firstDayDisplayed.set(Calendar.DAY_OF_WEEK, firstDayDisplayed.get(Calendar.DAY_OF_WEEK) -1);
+      firstDayDisplayed.setTimeInMillis( firstDayDisplayed.getTimeInMillis - DAY_LENGTH);
     }
     //and to the end of the week according to the locale set
     while(lastDayDisplayed.get(Calendar.DAY_OF_WEEK) != 7) {
-      lastDayDisplayed.set(Calendar.DAY_OF_WEEK, lastDayDisplayed.get(Calendar.DAY_OF_WEEK) +1);
+      lastDayDisplayed.setTimeInMillis(lastDayDisplayed.getTimeInMillis + DAY_LENGTH);
     }
-    val DAY_LENGTH = 1000l*60l*60l*24l
-    val days = (lastDayDisplayed.getTimeInMillis() - lastDayDisplayed.getTimeInMillis()) / DAY_LENGTH
+
+    val days = (lastDayDisplayed.getTimeInMillis() - firstDayDisplayed.getTimeInMillis()) / DAY_LENGTH
+
     val currentDate = makeDay(firstDayDisplayed)
-    var pickerWeek:PickerWeek = null
+    var pickerWeek:PickerWeek = new PickerWeek()
 
     /*beginning of a new week*/
     def newWeek() {
@@ -115,7 +122,7 @@ case class PickerMonth(var selectedDay: Calendar) {
       currentDate.setTimeInMillis(currentDate.getTimeInMillis+DAY_LENGTH)
       val currentPickerDay = new PickerDay
       currentPickerDay.day = makeDay(currentDate)
-      if (currentDate.get(Calendar.DAY_OF_WEEK) == 0) {
+      if (currentDate.get(Calendar.DAY_OF_WEEK) == 1) {
         currentPickerDay.firstDayOfWeek = true
         newWeek()
         //todo add holiday handling here with a holiday hashmap provided
@@ -132,7 +139,7 @@ case class PickerMonth(var selectedDay: Calendar) {
     val ret = Calendar.getInstance()
     ret.set(Calendar.YEAR, in.get(Calendar.YEAR))
     ret.set(Calendar.MONTH, in.get(Calendar.MONTH))
-    ret.set(Calendar.DAY_OF_MONTH, 0)
+    ret.set(Calendar.DAY_OF_MONTH, in.get(Calendar.DAY_OF_MONTH))
     ret.set(Calendar.HOUR, 0)
     ret.set(Calendar.MINUTE, 0)
     ret.set(Calendar.SECOND, 0)
--- src/test/scala/org/extrasapache/myfaces/codi/examples/ebean/support/ui/components/datePicker/PickerMonthTest.scala
+++ src/test/scala/org/extrasapache/myfaces/codi/examples/ebean/support/ui/components/datePicker/PickerMonthTest.scala
@@ -22,16 +22,21 @@ class PickerMonthTest {
     logger.info("Starting Test")
     var probe = Calendar.getInstance();
     probe.set(Calendar.YEAR, 2011)
-    probe.set(Calendar.MONTH, 1)
+    probe.set(Calendar.MONTH, Calendar.JANUARY)
     probe.set(Calendar.DAY_OF_WEEK, 2)
 
-    var pickerMonth = new PickerMonth(Calendar.getInstance())
+    var pickerMonth = new PickerMonth(probe)
 
     import DatePickerConversions._
 
-    //TODO first day 27.12.2010
-    logger.info("First:"+pickerDay2Calendar(pickerMonth.first).get(Calendar.DAY_OF_MONTH))
-    logger.info("Last:"+pickerDay2Calendar(pickerMonth.last).get(Calendar.DAY_OF_MONTH))
+    logger.info("First:"+pickerMonth.first.get(Calendar.DAY_OF_MONTH))
+    logger.info("Last:"+pickerMonth.last.get(Calendar.DAY_OF_MONTH))
+
+    assertTrue("First day", 27 == pickerMonth.first.get(Calendar.DAY_OF_MONTH))
+    assertTrue("Last day", 5 == pickerMonth.last.get(Calendar.DAY_OF_MONTH))
+
+    assertTrue("Month", Calendar.DECEMBER == pickerMonth.first.get(Calendar.MONTH))
+    assertTrue("Month", Calendar.FEBRUARY == pickerMonth.last.get(Calendar.MONTH))
 
     //Last day 06.02.2011
 
