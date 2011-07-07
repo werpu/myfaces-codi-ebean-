421a3a5c718a476a5548c688a24ce38940fed07d
Werner Punz <werner.punz@gmail.com>
2011-07-07 16:59:53 +0200

    ongoing works on the date picker, working on the data structure

--- src/main/scala/org/extrasapache/myfaces/codi/examples/ebean/support/ui/components/datePicker/DatePicker.scala
+++ src/main/scala/org/extrasapache/myfaces/codi/examples/ebean/support/ui/components/datePicker/DatePicker.scala
@@ -2,6 +2,8 @@ package org.extrasapache.myfaces.codi.examples.ebean.support.ui.components.dateP
 
 import javax.faces.component.FacesComponent
 import org.extrasapache.myfaces.codi.examples.ebean.support.ui.components.common.StandardJavascriptComponent
+import javax.faces.event.ComponentSystemEvent
+
 /**
  *
  * @author Werner Punz (latest modification by $Author$)
@@ -28,98 +30,16 @@ class DatePicker extends StandardJavascriptComponent {
   import java.util._
   import DatePicker._
 
-  var displayData = new ArrayList[PickerWeek]
-
-
-
-  /**
-   * prerender creates the date picker list model which then is used
-   * for further processing
-   */
-  def prepareMonth() {
-
-    val currentDay = getAttr[Calendar](VALUE, Calendar.getInstance())
-    //not render from beginning of the month to the end of the current month
-    var month = currentDay.get(Calendar.MONTH);
-    //now reset th
-    val currentMonth = makeMonth(currentDay)
-
-    //not iterate over all days and create the day entry list
-    val firstDay = currentMonth.getActualMinimum(Calendar.DAY_OF_MONTH)
-    var lastDay = currentMonth.getActualMaximum(Calendar.DAY_OF_MONTH)
-
-    val firstDayDisplayed = makeMonth(currentMonth);
-    firstDayDisplayed.set(Calendar.DAY_OF_MONTH, firstDay)
-
-    val lastDayDisplayed = makeMonth(currentMonth);
-    lastDayDisplayed.set(Calendar.DAY_OF_MONTH, firstDay)
-
-    //we now skip to the beginning of the week according to the locale set
-    while(firstDayDisplayed.get(Calendar.DAY_OF_WEEK) != 0) {
-      firstDayDisplayed.set(Calendar.DAY_OF_WEEK, firstDayDisplayed.get(Calendar.DAY_OF_WEEK) -1);
+  var displayData:PickerMonth = _
+  //we use prerender to enable the value conversion
+  //TODO move this into an implicit jsf converter in the long run
+  def preRenderInput(ev: ComponentSystemEvent) {
+    var value = getAttr[Calendar]("value", Calendar.getInstance())
+    if (displayData == null || displayData.selectedDay != value) {
+       displayData = new PickerMonth(value)
     }
-    //and to the end of the week according to the locale set
-    while(lastDayDisplayed.get(Calendar.DAY_OF_WEEK) != 6) {
-      lastDayDisplayed.set(Calendar.DAY_OF_WEEK, lastDayDisplayed.get(Calendar.DAY_OF_WEEK) +1);
-    }
-    val DAY_LENGTH = 1000l*60l*60l*24l
-    val days = (lastDayDisplayed.getTimeInMillis() - lastDayDisplayed.getTimeInMillis()) / DAY_LENGTH
-    val currentDate = makeDay(firstDayDisplayed)
-    var pickerWeek:PickerWeek = null
-
-    /*beginning of a new week*/
-    def newWeek() {
-      if (pickerWeek != null) {
-          displayData.add(pickerWeek)
-          pickerWeek = new PickerWeek
-        }
-    }
-
-    //we now increment all days and generate the meta data for our view
-    for(cnt <- 0 until days.asInstanceOf[Int] ) {
-      currentDate.setTimeInMillis(currentDate.getTimeInMillis+DAY_LENGTH)
-      val currentPickerDay = new PickerDay
-      currentPickerDay.day = makeDay(currentDate)
-      if (currentDate.get(Calendar.DAY_OF_WEEK) == 0) {
-        currentPickerDay.firstDayOfWeek = true
-        newWeek()
-        //todo add holiday handling here with a holiday hashmap provided
-      }
-      pickerWeek.days.add(currentPickerDay)
-    }
-    newWeek()
-    pickerWeek = null
-
-    //now that we have determined the starting and end day, we now generate the
-    //calendar data structures for displaying the meta information
-    //for loop between first day set and last day
   }
 
-  def makeMonth(in: Calendar): Calendar = {
-    val ret = Calendar.getInstance()
-    ret.set(Calendar.YEAR, in.get(Calendar.YEAR))
-    ret.set(Calendar.MONTH, in.get(Calendar.MONTH))
-    ret.set(Calendar.DAY_OF_MONTH, 0)
-    ret.set(Calendar.HOUR, 0)
-    ret.set(Calendar.MINUTE, 0)
-    ret.set(Calendar.SECOND, 0)
-    ret.set(Calendar.MILLISECOND, 0)
-
-    ret
-  }
-
-  def makeDay(in: Calendar): Calendar = {
-    val ret = Calendar.getInstance()
-    ret.set(Calendar.YEAR, in.get(Calendar.YEAR))
-    ret.set(Calendar.MONTH, in.get(Calendar.MONTH))
-    ret.set(Calendar.DAY_OF_MONTH, in.get(Calendar.DAY_OF_MONTH))
-    ret.set(Calendar.HOUR, 0)
-    ret.set(Calendar.MINUTE, 0)
-    ret.set(Calendar.SECOND, 0)
-    ret.set(Calendar.MILLISECOND, 0)
-
-    ret
-  }
 
 
 }
---
+++ src/main/scala/org/extrasapache/myfaces/codi/examples/ebean/support/ui/components/datePicker/PickerMonth.scala
@@ -0,0 +1,154 @@
+package org.extrasapache.myfaces.codi.examples.ebean.support.ui.components.datePicker
+
+import java.util.{ArrayList, Calendar}
+import java.util.logging.Logger
+
+/**
+ *
+ * @author Werner Punz (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+@serializable
+case class PickerMonth(var selectedDay: Calendar) {
+
+  @transient
+  var log = Logger.getLogger("PickerMonth")
+
+   
+  var data = prepareMonth(selectedDay)
+
+  /*properties for day month year selection*/
+  def day: Int = {
+    selectedDay.get(Calendar.DAY_OF_MONTH)
+  }
+
+  def day_$eq(day: Int) {
+    selectedDay.set(Calendar.DAY_OF_MONTH, day)
+  }
+
+  def month: Int = {
+    selectedDay.get(Calendar.MONTH)
+  }
+
+  def month_$eq(month: Int) {
+    selectedDay.set(Calendar.MONTH, month)
+    data = prepareMonth(selectedDay)
+  }
+
+  def year: Int = {
+    selectedDay.get(Calendar.YEAR)
+  }
+
+  def year_$eq(year: Int) {
+     selectedDay.set(Calendar.YEAR, year)
+     data = prepareMonth(selectedDay)
+  }
+
+  def nextMonth() {
+    this.month = this.month+1
+  }
+
+  def previousMonth() {
+    this.month = this.month-1
+  }
+
+  def nextYear() {
+    this.year = this.year+1
+  }
+
+  def previousYear() {
+    this.year = this.year-1
+  }
+
+
+
+  /**
+   * prerender creates the date picker list model which then is used
+   * for further processing
+   */
+  protected def prepareMonth(currentDay: Calendar): ArrayList[PickerWeek] = {
+
+    val ret = new ArrayList[PickerWeek]
+
+    val currentMonth = makeMonth(currentDay)
+
+    //not iterate over all days and create the day entry list
+    val firstDay = currentMonth.getActualMinimum(Calendar.DAY_OF_MONTH)
+
+    val firstDayDisplayed = makeMonth(currentMonth);
+    firstDayDisplayed.set(Calendar.DAY_OF_MONTH, firstDay)
+
+    val lastDayDisplayed = makeMonth(currentMonth);
+    lastDayDisplayed.set(Calendar.DAY_OF_MONTH, firstDay)
+
+    //we now skip to the beginning of the week according to the locale set
+    while(firstDayDisplayed.get(Calendar.DAY_OF_WEEK) != 1) {
+       log.info(firstDayDisplayed.get(Calendar.DAY_OF_WEEK).toString)
+      firstDayDisplayed.set(Calendar.DAY_OF_WEEK, firstDayDisplayed.get(Calendar.DAY_OF_WEEK) -1);
+    }
+    //and to the end of the week according to the locale set
+    while(lastDayDisplayed.get(Calendar.DAY_OF_WEEK) != 7) {
+      lastDayDisplayed.set(Calendar.DAY_OF_WEEK, lastDayDisplayed.get(Calendar.DAY_OF_WEEK) +1);
+    }
+    val DAY_LENGTH = 1000l*60l*60l*24l
+    val days = (lastDayDisplayed.getTimeInMillis() - lastDayDisplayed.getTimeInMillis()) / DAY_LENGTH
+    val currentDate = makeDay(firstDayDisplayed)
+    var pickerWeek:PickerWeek = null
+
+    /*beginning of a new week*/
+    def newWeek() {
+      if (pickerWeek != null) {
+          ret.add(pickerWeek)
+          pickerWeek = new PickerWeek
+        }
+    }
+    log.info("Days" + days.asInstanceOf[Int])
+    //we now increment all days and generate the meta data for our view
+    for(cnt <- 0 until days.asInstanceOf[Int] ) {
+      currentDate.setTimeInMillis(currentDate.getTimeInMillis+DAY_LENGTH)
+      val currentPickerDay = new PickerDay
+      currentPickerDay.day = makeDay(currentDate)
+      if (currentDate.get(Calendar.DAY_OF_WEEK) == 0) {
+        currentPickerDay.firstDayOfWeek = true
+        newWeek()
+        //todo add holiday handling here with a holiday hashmap provided
+      }
+      pickerWeek.days.add(currentPickerDay)
+    }
+    newWeek()
+    pickerWeek = null
+
+    ret
+  }
+
+  protected def makeMonth(in: Calendar): Calendar = {
+    val ret = Calendar.getInstance()
+    ret.set(Calendar.YEAR, in.get(Calendar.YEAR))
+    ret.set(Calendar.MONTH, in.get(Calendar.MONTH))
+    ret.set(Calendar.DAY_OF_MONTH, 0)
+    ret.set(Calendar.HOUR, 0)
+    ret.set(Calendar.MINUTE, 0)
+    ret.set(Calendar.SECOND, 0)
+    ret.set(Calendar.MILLISECOND, 0)
+
+    ret
+  }
+
+  protected def makeDay(in: Calendar): Calendar = {
+    val ret = Calendar.getInstance()
+    ret.set(Calendar.YEAR, in.get(Calendar.YEAR))
+    ret.set(Calendar.MONTH, in.get(Calendar.MONTH))
+    ret.set(Calendar.DAY_OF_MONTH, in.get(Calendar.DAY_OF_MONTH))
+    ret.set(Calendar.HOUR, 0)
+    ret.set(Calendar.MINUTE, 0)
+    ret.set(Calendar.SECOND, 0)
+    ret.set(Calendar.MILLISECOND, 0)
+
+    ret
+  }
+
+  def main(args: Array[String]) {
+    var pickerMonth = new PickerMonth(Calendar.getInstance());
+  }
+
+}
