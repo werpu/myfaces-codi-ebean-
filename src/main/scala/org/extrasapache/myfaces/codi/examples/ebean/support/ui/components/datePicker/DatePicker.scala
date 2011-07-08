a372e02227b8504acd63966b72612a14a9e416ad
Werner Punz <werner.punz@gmail.com>
2011-07-08 21:53:41 +0200

    date picker save

--- src/main/scala/org/extrasapache/myfaces/codi/examples/ebean/support/ui/components/datePicker/DatePicker.scala
+++ src/main/scala/org/extrasapache/myfaces/codi/examples/ebean/support/ui/components/datePicker/DatePicker.scala
@@ -2,8 +2,8 @@ package org.extrasapache.myfaces.codi.examples.ebean.support.ui.components.dateP
 
 import javax.faces.component.FacesComponent
 import org.extrasapache.myfaces.codi.examples.ebean.support.ui.components.common.StandardJavascriptComponent
-import javax.faces.event.{ActionEvent, ComponentSystemEvent}
 import java.util.logging.Logger
+import javax.faces.event.{PostRestoreStateEvent, ListenerFor, ActionEvent, ComponentSystemEvent}
 
 /**
  *
@@ -12,12 +12,12 @@ import java.util.logging.Logger
  */
 
 object DatePicker {
-  val DATE_PATTERN  = "datePattern"
-  val HOLIDAYS      = "holidays"
-  val SPECIAL_DAYS  = "specialDays"
+  val DATE_PATTERN = "datePattern"
+  val HOLIDAYS = "holidays"
+  val SPECIAL_DAYS = "specialDays"
 
-  val BEG_WEEK      = "beginningOfWeek"
-  val VALUE         = "value"
+  val BEG_WEEK = "beginningOfWeek"
+  val VALUE = "value"
 }
 
 /**
@@ -26,26 +26,69 @@ object DatePicker {
  */
 @FacesComponent("at.irian.DatePicker")
 @serializable
+@ListenerFor(systemEventClass = classOf[PostRestoreStateEvent])
 class DatePicker extends StandardJavascriptComponent {
 
   import java.util._
   import DatePicker._
 
-  var displayData:PickerMonth = _
-  //we use prerender to enable the value conversion
-  //TODO move this into an implicit jsf converter in the long run
-  def preRenderInput(ev: ComponentSystemEvent) {
-    var value = getAttr[Calendar]("value", Calendar.getInstance())
+  var displayData: PickerMonth = _
+
+  /*
+   * the listener now is responsible for checking for an incoming
+   * date change value and then parsing it in
+   */
+  override def processEvent(event: ComponentSystemEvent) {
+    val logger = Logger.getLogger("DatePicker.selectDay")
+    logger.info("decoded")
+    val incomingParam: String = reqAttrMap.get("mf_dp")
+    if (incomingParam != null && incomingParam.trim != "") {
+      val longVal:Long =  incomingParam.toLong
+      val value = getAttr[Calendar](VALUE, Calendar.getInstance())
+      value.setTimeInMillis(longVal)
+      setAttr[Calendar](VALUE,value)
+    }
+    restoreDisplayData
+    super.processEvent(event)
+  }
+
+  protected def restoreDisplayData {
+    val value = getAttr[Calendar]("value", Calendar.getInstance())
     if (displayData == null || displayData.selectedDay != value) {
-       displayData = new PickerMonth(value)
+      displayData = new PickerMonth(value)
     }
   }
 
-  def selectDay:String = {
+  def nextYear(event: ActionEvent) {
     val logger = Logger.getLogger("DatePicker.selectDay")
-    logger.info("picked")
-    return null
+    logger.info("nextYear")
   }
 
+  def nextMonth(event: ActionEvent) {
+    val logger = Logger.getLogger("DatePicker.selectDay")
+    logger.info("nextMonth")
+  }
+
+  def previousMonth(event: ActionEvent) {
+    val logger = Logger.getLogger("DatePicker.selectDay")
+    logger.info("previousMonth")
+  }
+
+  def previousYear(event: ActionEvent) {
+    val logger = Logger.getLogger("DatePicker.selectDay")
+    logger.info("previousYear")
+  }
+
+  def preRenderInput(ev: ComponentSystemEvent) {
+    //we fetch the request attribute in case we did not have it yet
+
+    restoreDisplayData
+  }
+
+  def selectDay: String = {
+    val logger = Logger.getLogger("DatePicker.selectDay")
+    logger.info("picked")
+    null
+  }
 
 }
