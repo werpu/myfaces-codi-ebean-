12dfc54dc1f0ee8f07d4c511a415be575b800db1
Werner Punz <werner.punz@gmail.com>
2011-08-12 16:01:49 +0200

    externalizing constants

--- plugin/src/main/java/org/apache/myfaces/plugins/jsdoc/JSDocMojo.java
+++ plugin/src/main/java/org/apache/myfaces/plugins/jsdoc/JSDocMojo.java
@@ -33,14 +33,23 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import static org.apache.myfaces.plugins.jsdoc.JSDocMojoConst.*;
+
 /**
  * @author Werner Punz (latest modification by $Author$)
  * @version $Revision$ $Date$
  *          <p/>
  *          A simple jsdoc plugin which should cover our documentation needs
+ *          Note this plugin is a simplified tailored derivate from
+ *          <p/>
+ *          http://www.abiss.gr some code stems from there.
+ *          <p/>
+ *          since we use jsdoc for now and are not in the reporting part
+ *          a simple plugin suffices.
  * @goal jsdoc
  */
 public class JSDocMojo extends AbstractMojo {
+
 
     /**
      * the root project build dir (target directory)
@@ -69,29 +78,35 @@ public class JSDocMojo extends AbstractMojo {
      *
      * @parameter expression="false"
      */
-    private boolean includePrivate;
+    boolean includePrivate;
 
     /**
      * Include all functions, even undocumented ones. Default is <code>false</code>.
      *
      * @parameter expression="false"
      */
-    private boolean includeUndocumented;
+    boolean includeUndocumented;
 
     /**
      * Include all functions, even undocumented, underscored ones. Default is <code>false</code>.
      *
      * @parameter expression="false"
      */
-    private boolean includeUndocumentedUnderscored;
+     boolean includeUndocumentedUnderscored;
 
-
-
-     /**
+    /**
      * template directory used by jsdoc the default is <code>templates/jsdoc</code> under the jsdoc root
+     *
      * @parameter expression="templates/jsdoc"
      */
-    private String templates;
+     String templates;
+
+    /**
+     * output dir override
+     *
+     * @parameter expression=""
+     */
+     String outputDirectory;
 
     /**
      * the parsed xml filemap containing the single source files
@@ -113,9 +128,9 @@ public class JSDocMojo extends AbstractMojo {
             throw new MojoExecutionException(e.toString());
         }
         //unpacker = new JSDocUnpackerMaven()
-        unpacker = new JSDocPackMaven();
+        unpacker = new JSDocPackResources();
 
-        jsdocTargetPath = projectBuildDir + File.separator + "temp" + File.separator + "jsdoc";
+        jsdocTargetPath = projectBuildDir + File.separator + TEMP + File.separator + JSDOC;
         File pathCreator = new File(jsdocTargetPath);
         pathCreator.mkdirs();
     }
@@ -132,45 +147,56 @@ public class JSDocMojo extends AbstractMojo {
         unpacker.unpack(jsdocTargetPath, getLog());
         getLog().info("[JSDOC] Unpacking jsdoc toolkit for further processing done");
 
-        String systemJsdocDir = System.getProperty("jsdoc.dir");
-        System.setProperty("jsdoc.dir", jsdocTargetPath);
+        String systemJsdocDir = System.getProperty(JSDOC_DIR);
+        System.setProperty(JSDOC_DIR, jsdocTargetPath);
 
+        List<String> args = _initArguments();
+
+        getLog().info("[JSDOC] Executing within maven: '" + args.toString().replaceAll(",", "") + "'");
+
+        // tell Rhino to run JSDoc with the provided params
+        // without calling System.exit
+        org.mozilla.javascript.tools.shell.Main.main(args.toArray(new String[0]));
+        if (systemJsdocDir != null) {
+            System.setProperty(JSDOC_DIR, systemJsdocDir);
+        }
+    }
+
+    private final List<String> _initArguments() {
         List<String> args = new ArrayList<String>();
-        String runJsPath = jsdocTargetPath +File.separator+"app" + File.separator + "run.js";
+        String runJsPath = jsdocTargetPath + File.separator + APP + File.separator + RUN_JS;
         args.add(runJsPath);
 
         if (this.includeUndocumented) {
-            args.add("-a");
+            args.add(PARAM_UNDOCUMENTED);
         }
         if (this.includeUndocumentedUnderscored) {
-            args.add("-A");
+            args.add(PARAM_UNDOCUMENTED_UNDERSCORED);
         }
         if (this.includePrivate) {
-            args.add("-p");
+            args.add(PARAM_PRIVATE);
         }
-        args.add("-d=" + this.getOutputDirectory());
-        args.add("-t=" + getTemplateDirectory());
+        args.add(PARAM_OUTPUT + EQUALS + this.getOutputDirectory());
+        args.add(PARAM_TEMPLATE + EQUALS + getTemplateDirectory());
+
         args.addAll(getSources());
-         //according to the run.js source the last argument
+        //according to the run.js source the last argument
         //must be a -j param pointing to the jsdoc javascripts
-		args.add("-j=" + runJsPath);
-
-        getLog().info("[JSDOC] Executing within maven: '" + args.toString().replaceAll(",","") + "'");
-
-		// tell Rhino to run JSDoc with the provided params
-		// without calling System.exit
-		org.mozilla.javascript.tools.shell.Main.main(args.toArray(new String[0]));
-        if (systemJsdocDir != null) {
-            System.setProperty("jsdoc.dir", systemJsdocDir);
-        }
+        args.add(PARAM_JS_FLAG + EQUALS + runJsPath);
+        return args;
     }
 
-    private String getTemplateDirectory() {
-        return jsdocTargetPath + File.separator + this.templates;
+    private final String getTemplateDirectory() {
+        return (TEMPLATES_JSDOC.equals(this.templates)) ?
+                this.jsdocTargetPath + File.separator + this.templates :
+                this.templates;
     }
 
-    private String getOutputDirectory() {
-        return projectBuildDir+File.separator+"jsdoc";
+    private final String getOutputDirectory() {
+        return (this.outputDirectory == null || this.outputDirectory.equals("")) ?
+                projectBuildDir + File.separator + JSDOC :
+                this.outputDirectory;
+
     }
 
     private List<String> getSources() {
